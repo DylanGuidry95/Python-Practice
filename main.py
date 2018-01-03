14e49ffbe5b5edd86c12a302f30acbef1cb806e2
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-01-03 15:49:02 -0600

    Year One Practice

---
+++ main.py
@@ -0,0 +1,21 @@
+"""this is a test"""
+
+def _add(_a, _b):
+    return _a + _b
+
+print "Hello World"
+A = 1
+print A
+B = 2
+print A + B
+print _add(A, B)
+
+X = 1
+if X == 1:
+    print "true"
+else:
+    print "false"
+
+for I in range(0, 100):
+    print I
+    
