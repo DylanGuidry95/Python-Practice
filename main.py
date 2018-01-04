0deda944e0c02cbe1f0f88dd5b5b312fd841c6a8
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-01-04 10:11:53 -0600

    calculator

--- main.py
+++ main.py
@@ -18,4 +18,18 @@ else:
 
 for I in range(0, 100):
     print I
-    
+
+EXIT = ' '
+while EXIT != 'q':
+    LHS = int(raw_input("Insert a value for the lhs: "))
+    RHS = int(raw_input("Insert a value for the rhs: "))
+    SIGN = raw_input("Math operation you want to perform add, sub, mult, div: ")
+    if SIGN == "add":
+        print LHS + RHS
+    elif SIGN == "sub":
+        print LHS - RHS
+    elif SIGN == "mult":
+        print LHS * RHS
+    elif SIGN == "div":
+        print LHS / RHS
+    EXIT = raw_input("Press any key to use again or q to exit")
