a034b77c7703030d2dd9e7e09d81e3d6f578c31e
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-01-05 17:54:16 -0600

    more grid

--- grid.py
+++ grid.py
@@ -1,6 +1,19 @@
 class GridVisual(object):
     def __init__(self):
-        self.cell_visuals = []
+        self.cell_visuals = []        
 
     def add_cells(self, cells):
         self.cell_visuals = cells
+
+    def draw(self, screen):
+        for cell in self.cell_visuals:
+            cell.draw(screen)
+
+class Grid(object):
+    def __init__(self, w, h)
+        self.width = w
+        self.height = h
+        self.cells = []
+        self.open_list = []
+        self.closed_list = []
+            
