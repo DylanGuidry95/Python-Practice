e1101e75fb04241feec4e9b227c1890cbec4171e
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-01-26 15:49:04 -0600

    Comments

--- drawingmain.py
+++ drawingmain.py
@@ -1,19 +1,20 @@
+'''Main file for testing drawing utils'''
 #pylint: disable=E1101
 
 import pygame
 from shapes import Rectangle
 from shapes import Circle
 from shapes import Text
-from vector2 import vector2
+from vector2 import Vector2
 
 pygame.init()
 
 SCREEN = pygame.display.set_mode((1080, 720))
 SCREEN.fill((255, 255, 255))
 
-RECT = Rectangle(vector2(100, 100), (0, 255, 0), (100, 100), SCREEN)
-CIRC = Circle(vector2(400, 400), (255, 0, 0), 50, SCREEN)
-TEXT = Text(vector2(10, 10), (255, 0, 0), "Hello World", 24, SCREEN)
+RECT = Rectangle(Vector2(100, 100), (0, 255, 0), (100, 100), SCREEN)
+CIRC = Circle(Vector2(400, 400), (255, 0, 0), 50, SCREEN)
+TEXT = Text(Vector2(10, 10), (255, 0, 0), "Hello World", 24, SCREEN)
 
 
 
