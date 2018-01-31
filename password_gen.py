ad0ca182329d59701faaecfa20e53bd2b49ce12e
Dylan Guidry <dylan.guidry@aie.edu.au>
2018-01-31 10:28:38 -0600

    password generation

--- password_gen.py
+++ password_gen.py
@@ -16,14 +16,38 @@ def password_gen(security):
         words.append(current_list)
 
     password = ""
-    if security == 1:
+    for i in range(security):
         index_a = randint(0, len(words) - 1)
         index_b = randint(0, len(words[index_a]) - 1)
         password += words[index_a][index_b]
-    if security == 2:
-        index = []
-        for i in range
 
-    print password
+    conversions = open("conversions.txt")    
+    for letter in conversions:
+        letter = letter.replace("\n", "")
+        id = letter.split('-')
+        if password.__contains__(id[0]):            
+            password = password.replace(str(id[0]), str(id[1]))
+            
+    contains_number = False
+    for number in range(10):
+        if password.__contains__(str(number)):
+            contains_number = True
 
-password_gen(1)
+    if contains_number == False:
+        password += str(randint(0, 9999))
+
+    contains_cap_letter = False
+    while contains_cap_letter == False:
+        index = randint(0, len(password) - 1)
+        if ord(password[index]) >= 97 and ord(password[index]) <= 122:
+            password = password.replace(password[index], chr(ord(password[index]) - 32))
+            contains_cap_letter = True
+
+    passwords = open("pass.txt", 'a')
+    passwords.writelines(password)
+    passwords.write("\n")
+    passwords.close()
+
+    return password
+
+password_gen(3)
